e04068fab1e1ceda5c0fb7280ba4cf4e5bb1e8ad
Weimin Ye <billyinusa@gmail.com>
2014-12-15 23:43:15 +0800

    Add Uncategorized.cs into BDD.API

---
+++ jqgrid-asp.net-mvc/jqgrid-asp.net-mvc.Tests.TDD/Steps/Uncategorized.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using TechTalk.SpecFlow;
+
+namespace jqgrid_asp.net_mvc.Tests.TDD.API.Steps
+{
+    [Binding]
+    public class Uncategorized
+    {
+    }
+}
