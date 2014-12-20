2c40b578466feb9babec0e132c37c13b31b333e5
Weimin Ye <billyinusa@gmail.com>
2014-12-21 01:24:49 +0800

    replace to using jqgrid_asp.net_mvc; at controller class header

--- jqgrid-asp.net-mvc/jqgrid-asp.net-mvc.demo.web/Controllers/HomeController.cs
+++ jqgrid-asp.net-mvc/jqgrid-asp.net-mvc.demo.web/Controllers/HomeController.cs
@@ -4,7 +4,7 @@ using System.Linq;
 using System.Web;
 using System.Web.Mvc;
 using jqgrid_asp.net_mvc.demo.web.Models;
-using jqgrid_asp.net_mvc.Helpers;
+using jqgrid_asp.net_mvc;
 
 namespace jqgrid_asp.net_mvc.demo.web.Controllers
 {
